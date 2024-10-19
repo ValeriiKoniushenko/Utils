240c6d3031b6afd936120a0a9e34b2bdd0e2cc2e
ValeriiKoniushenko <Valerii.Koniushenko@gmail.com>
2024-10-19 18:55:12 +0300

    fixed bugs & refactored

--- sources/Core/String.h
+++ sources/Core/String.h
@@ -298,7 +298,7 @@ namespace Core
         std::is_same_v<std::decay_t<T>, int> || std::is_same_v<std::decay_t<T>, double> || std::is_same_v<std::decay_t<T>, float> ||
         std::is_same_v<std::decay_t<T>, unsigned long long> || std::is_same_v<std::decay_t<T>, const char*> ||
         std::is_same_v<std::decay_t<T>, char*> || std::is_same_v<std::decay_t<T>, const wchar_t*> || std::is_same_v<std::decay_t<T>, wchar_t*> ||
-        std::is_same_v<T, BaseString<T>>;
+        std::is_same_v<BaseString<T::CharT>, T>;
 
     template<class CharType>
     class BaseString : public Utils::CopyableAndMoveable
