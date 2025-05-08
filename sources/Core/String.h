56147ce0d97edb0e6110496f8bea6ca7af188d77
Valerii Koniushenko <Valerii.Koniushenko@gmail.com>
2025-05-08 16:04:01 +0200

    Fixed small typos & updated several functoins(refactoring)

--- sources/Core/String.h
+++ sources/Core/String.h
@@ -1,24 +1,24 @@
-// MIT License
+//  MIT License
 //
-// Copyright (c) 2024 Valerii Koniushenko
+//  Copyright (c) 2019-2025 Valerii Koniushenko
 //
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
 //
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
 //
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
 
 #pragma once
 
@@ -1403,7 +1403,7 @@ namespace Core
         {
             if (!isEmpty())
             {
-                return regexFind(std::move(expr), offset, limit, matchOptions, compileOptions | PCRE2_ANCHORED).isMatched();
+                return regexFind(std::move(expr), offset, limit, matchOptions | PCRE2_ANCHORED, compileOptions).isMatched();
             }
 
             return false;
@@ -2191,7 +2191,7 @@ namespace Core
         static constexpr SizeT _capacityMultiplier = 2ull;
 
     private:
-        // ================== PIPMPLs =======================
+        // ================== PIMPLs =======================
         template<bool IsConst, class FuncT>
         static void ForEachByLineImpl(AdaptiveRawPtr<IsConst> base, FuncT&& callback, LineSeparator separator)
         {
