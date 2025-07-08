9bd4cb6ce0f986feb44336c986b18198a8b64c96
Valerii Koniushenko <Valerii.Koniushenko@gmail.com>
2025-07-08 22:51:04 +0200

    Moved from git-submodules to cmake deps

---
+++ cmake/FetchDependencies.cmake
@@ -0,0 +1,47 @@
+include(FetchContent)
+
+FetchContent_Declare(Glm
+        GIT_REPOSITORY https://github.com/g-truc/glm.git
+        GIT_TAG 1.0.1
+        GIT_SHALLOW TRUE
+        GIT_PROGRESS TRUE
+)
+set(GLM_TEST_ENABLE OFF CACHE BOOL "" FORCE)
+
+FetchContent_MakeAvailable(Glm)
+
+
+
+FetchContent_Declare(GoogleTest
+        GIT_REPOSITORY https://github.com/google/googletest.git
+        GIT_TAG v1.17.0
+        GIT_SHALLOW TRUE
+        GIT_PROGRESS TRUE
+)
+set(INSTALL_GTEST OFF CACHE BOOL "" FORCE)
+
+FetchContent_MakeAvailable(GoogleTest)
+
+
+
+FetchContent_Declare(Benchmark
+        GIT_REPOSITORY https://github.com/google/benchmark.git
+        GIT_TAG v1.9.4
+        GIT_SHALLOW TRUE
+        GIT_PROGRESS TRUE
+)
+
+FetchContent_MakeAvailable(Benchmark)
+
+
+
+FetchContent_Declare(PCRE2
+        GIT_REPOSITORY https://github.com/PCRE2Project/pcre2.git
+        GIT_TAG pcre2-10.45
+        GIT_SHALLOW TRUE
+        GIT_PROGRESS TRUE
+)
+set(PCRE2_BUILD_TESTS OFF CACHE BOOL "" FORCE)
+set(PCRE2_BUILD_PCRE2GREP OFF CACHE BOOL "" FORCE)
+
+FetchContent_MakeAvailable(PCRE2)
