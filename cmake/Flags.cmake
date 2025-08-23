ba456d01b8231dc59645276620bf70b249d7ad71
Valerii Koniushenko <Valerii.Koniushenko@gmail.com>
2025-08-23 03:02:47 +0200

    Added more compiler flags

--- cmake/Flags.cmake
+++ cmake/Flags.cmake
@@ -4,15 +4,32 @@ function(UtilsCoreAddCompileOptionsTo Target)
     if (MSVC)
         target_compile_options(${Target} PRIVATE
             "/W4"
+            "/wd4005"            # disable: macro redefinition (closest to -Wno-comment, MSVC warns differently)
+            "/wd4101"            # disable: unused variable (-Wno-unused-variable)
+            "/wd4100"            # disable: unused parameter (-Wno-unused-parameter)
+            "/we4715"            # treat "not all control paths return a value" as error (-Werror=return-type)
+            "/wd4702"            # no unreachable code
+
+            # Debug config
+            "$<$<CONFIG:DEBUG>:/Zi>"
+            "$<$<CONFIG:DEBUG>:/Od>"
+            "$<$<CONFIG:DEBUG>:/Ob0>"
+            "$<$<CONFIG:DEBUG>:/Oy->"
+
+            # Release config
             "$<$<CONFIG:RELEASE>:/O2>"
         )
     else ()
         target_compile_options(${Target} PRIVATE
             "-Wall"
             "-Wextra"
+            "-Wno-comment"
             # "-Werror"
             "-Wno-error=unused-variable"
             "-Wno-unused-variable"
+            "-Wno-unused-parameter"
+            "-Werror=return-type"
+
             "$<$<CONFIG:DEBUG>:-g3>"
             "$<$<CONFIG:DEBUG>:-O0>"
             "$<$<CONFIG:DEBUG>:-fno-inline>"
