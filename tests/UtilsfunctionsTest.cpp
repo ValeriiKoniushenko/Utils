951fb51414c41d8358eb314ea941f93bf8ac9001
Valerii Koniushenko <Valerii.Koniushenko@gmail.com>
2025-08-23 03:18:50 +0200

    Covered more tests

--- tests/UtilsfunctionsTest.cpp
+++ tests/UtilsfunctionsTest.cpp
@@ -69,3 +69,26 @@ and more recently with desktop publishing software like Aldus PageMaker includin
 
     std::filesystem::remove(correctPath);
 }
+
+TEST(UtilsTests, SimpleGetFileContent)
+{
+    const char* text = R"(
+Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+    It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+)";
+
+    const std::filesystem::path correctPath = "testFile.txt";
+
+    std::ofstream testFile(correctPath);
+    ASSERT_TRUE(testFile.is_open());
+    testFile.write(text, strlen(text));
+    testFile.close();
+
+    auto&& fileContent = Utils::GetFileContent(correctPath);
+    EXPECT_STREQ(text, fileContent.data());
+
+    std::filesystem::remove(correctPath);
+
+}
