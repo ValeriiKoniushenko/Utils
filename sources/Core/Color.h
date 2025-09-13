5e36db713ee88d9c282066e573bb54435b3894fa
Valerii Koniushenko <Valerii.Koniushenko@gmail.com>
2025-09-13 16:27:43 +0200

    Added more functionality and tests to Colors

--- sources/Core/Color.h
+++ sources/Core/Color.h
@@ -46,6 +46,8 @@ namespace Core
         {
         }
 
+        constexpr explicit Color4(const Color3& value, uint8_t alpha = 255);
+
         [[nodiscard]] constexpr static Color4 From(const NormColor4& other) noexcept;
         constexpr void from(const NormColor4& other) noexcept;
         [[nodiscard]] constexpr NormColor4 toNorm() const noexcept;
@@ -119,6 +121,11 @@ namespace Core
         return NormColor4::From(*this);
     }
 
+    constexpr Color4::Color4(const Color3& value, uint8_t alpha)
+        : Parent(value.r, value.g, value.b, alpha)
+    {
+    }
+
     constexpr Color4 Color4::From(const NormColor4& other) noexcept
     {
         Color4 out;
@@ -187,5 +194,68 @@ namespace Core
     using RGBA = Color4;
     using RGBn = NormColor3;
     using RGBAn = NormColor4;
+
+    // Default colors
+    static constinit auto Color3_Red = Color3(255, 0, 0);
+    static constinit auto Color3_Green = Color3(0, 255, 0);
+    static constinit auto Color3_Blue = Color3(0, 0, 255);
+    static constinit auto Color3_Yellow = Color3(255, 255, 0);
+    static constinit auto Color3_Cyan = Color3(0, 255, 255);
+    static constinit auto Color3_Magenta = Color3(255, 0, 255);
+    static constinit auto Color3_Orange = Color3(255, 165, 0);
+    static constinit auto Color3_Purple = Color3(128, 0, 128);
+    static constinit auto Color3_Gray = Color3(128, 128, 128);
+    static constinit auto Color3_White = Color3(255, 255, 255);
+    static constinit auto Color3_Black = Color3(0, 0, 0);
+    static constinit auto Color3_LightGray = Color3(211, 211, 211);
+    static constinit auto Color3_DarkGray = Color3(64, 64, 64);
+    static constinit auto Color3_Brown = Color3(139, 69, 19);
+    static constinit auto Color3_Tan = Color3(210, 180, 140);
+    static constinit auto Color3_Olive = Color3(128, 128, 0);
+    static constinit auto Color3_Lime = Color3(50, 205, 50);
+    static constinit auto Color3_SeaGreen = Color3(46, 139, 87);
+    static constinit auto Color3_Teal = Color3(0, 128, 128);
+    static constinit auto Color3_Turquoise = Color3(64, 224, 208);
+    static constinit auto Color3_SkyBlue = Color3(135, 206, 235);
+    static constinit auto Color3_DeepSkyBlue = Color3(0, 191, 255);
+    static constinit auto Color3_Navy = Color3(0, 0, 128);
+    static constinit auto Color3_Indigo = Color3(75, 0, 130);
+    static constinit auto Color3_Violet = Color3(238, 130, 238);
+    static constinit auto Color3_Pink = Color3(255, 192, 203);
+    static constinit auto Color3_HotPink = Color3(255, 105, 180);
+    static constinit auto Color3_Coral = Color3(255, 127, 80);
+    static constinit auto Color3_Gold = Color3(255, 215, 0);
+    static constinit auto Color3_Silver = Color3(192, 192, 192);
+
+    static constinit auto Color4_Red = Color4(255, 0, 0, 255);
+    static constinit auto Color4_Green = Color4(0, 255, 0, 255);
+    static constinit auto Color4_Blue = Color4(0, 0, 255, 255);
+    static constinit auto Color4_Yellow = Color4(255, 255, 0, 255);
+    static constinit auto Color4_Cyan = Color4(0, 255, 255, 255);
+    static constinit auto Color4_Magenta = Color4(255, 0, 255, 255);
+    static constinit auto Color4_Orange = Color4(255, 165, 0, 255);
+    static constinit auto Color4_Purple = Color4(128, 0, 128, 255);
+    static constinit auto Color4_Gray = Color4(128, 128, 128, 255);
+    static constinit auto Color4_White = Color4(255, 255, 255, 255);
+    static constinit auto Color4_Black = Color4(0, 0, 0, 255);
+    static constinit auto Color4_LightGray = Color4(211, 211, 211, 255);
+    static constinit auto Color4_DarkGray = Color4(64, 64, 64, 255);
+    static constinit auto Color4_Brown = Color4(139, 69, 19, 255);
+    static constinit auto Color4_Tan = Color4(210, 180, 140, 255);
+    static constinit auto Color4_Olive = Color4(128, 128, 0, 255);
+    static constinit auto Color4_Lime = Color4(50, 205, 50, 255);
+    static constinit auto Color4_SeaGreen = Color4(46, 139, 87, 255);
+    static constinit auto Color4_Teal = Color4(0, 128, 128, 255);
+    static constinit auto Color4_Turquoise = Color4(64, 224, 208, 255);
+    static constinit auto Color4_SkyBlue = Color4(135, 206, 235, 255);
+    static constinit auto Color4_DeepSkyBlue = Color4(0, 191, 255, 255);
+    static constinit auto Color4_Navy = Color4(0, 0, 128, 255);
+    static constinit auto Color4_Indigo = Color4(75, 0, 130, 255);
+    static constinit auto Color4_Violet = Color4(238, 130, 238, 255);
+    static constinit auto Color4_Pink = Color4(255, 192, 203, 255);
+    static constinit auto Color4_HotPink = Color4(255, 105, 180, 255);
+    static constinit auto Color4_Coral = Color4(255, 127, 80, 255);
+    static constinit auto Color4_Gold = Color4(255, 215, 0, 255);
+    static constinit auto Color4_Silver = Color4(192, 192, 192, 255);
 
 } // namespace Core
