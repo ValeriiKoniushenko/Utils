28810e6d086c55b84058376735e308a01750c091
Valerii Koniushenko <Valerii.Koniushenko@gmail.com>
2025-09-21 20:41:53 +0200

    Improved and refactored Jenkins builds & testing

---
+++ .jenkins/LinuxUnitTests_clang_debug.groovy
@@ -0,0 +1,38 @@
+pipeline {
+    agent { label 'Linux' }
+
+    environment {
+        C_COMPILER = 'clang'
+        CPP_COMPILER = 'clang++'
+        BUILD_TYPE = 'Debug'
+    }
+
+    stages {
+        stage('Run') {
+            steps {
+                script {
+                    copyArtifacts(
+                        projectName: 'LinuxBuild_Clang_Debug',
+                        selector: lastCompleted(),
+                        fingerprintArtifacts: true
+                    )
+                    sh """
+                        tar -xzvf ${C_COMPILER}-${BUILD_TYPE}.tar.gz
+
+                        cd build/${C_COMPILER}/${BUILD_TYPE}/bin
+                        ./UtilsTests --gtest_output=xml:gtest_result.xml
+                        cd ../../../../
+                    """
+                }
+            }
+        }
+
+        stage('Gen Test Results') {
+            steps {
+                script {
+                    junit "build/${C_COMPILER}/${BUILD_TYPE}/bin/gtest_result.xml"
+                }
+            }
+        }
+    }
+}
