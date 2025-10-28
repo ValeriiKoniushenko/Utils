9459a3bc23677f2dce99dff37d9751282c1445a0
Valerii Koniushenko <Valerii.Koniushenko@gmail.com>
2025-10-28 23:38:18 +0100

    Improved working with const objects for IntrusivePtr

--- sources/Core/IntrusivePtr.h
+++ sources/Core/IntrusivePtr.h
@@ -141,7 +141,7 @@ namespace Core
         IntrusivePtr(const IntrusivePtr<std::remove_const_t<T>>& other)
             requires std::is_const_v<T>
         {
-            *this = other;
+            (void)*this->operator=(reinterpret_cast<const IntrusivePtr&>(other));
         }
 
         IntrusivePtr(const IntrusivePtr& other) { *this = other; }
