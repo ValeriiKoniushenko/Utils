aac097ee958343dbd7ff4386ffff44b3ac8340e2
Valerii Koniushenko <Valerii.Koniushenko@gmail.com>
2026-03-27 22:13:37 +0100

    Added extra check for including 'stactrace'

--- sources/Core/BaseAssert.h
+++ sources/Core/BaseAssert.h
@@ -53,7 +53,8 @@ inline void Assert(bool condition, const char* message = nullptr)
 
     cerr << "Assert was got: " << endl
          << "Message: " << (message ? message : "None") << endl
-    #if defined(__cpp_lib_stacktrace) && !defined(DONT_USE_CPP_LIB_STACKTRACE)
+    #if defined(__cpp_lib_stacktrace) && !defined(DONT_USE_CPP_LIB_STACKTRACE)                         \
+        && !(defined(__clang__) && defined(__GLIBCXX__))
          << "Stacktrace: " << endl
          << std::stacktrace::current() << endl
     #endif
