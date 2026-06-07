07883a66a7caac08d0f259f8e824dd8063bc0134
Valerii Koniushenko <Valerii.Koniushenko@gmail.com>
2026-06-07 13:07:22 +0200

    refactor(CMakeLists.txt, cmake/FetchDependencies.cmake): suppress deprecation warnings and clean up dependency messages
    - Temporarily disabled `CMAKE_WARN_DEPRECATED` in `CMakeLists.txt` to suppress deprecation warnings during dependency inclusion.
    - Removed unnecessary space in the `message` function call in `Utils::FetchDependencies.cmake`.
    - Adjusted `Core_SuppressAllSubmoduleWarnings` calls for consistency and readability.

--- cmake/FetchDependencies.cmake
+++ cmake/FetchDependencies.cmake
@@ -1,6 +1,6 @@
 include(FetchContent)
 
-message(DEBUG "Utils::FetchDependencies.cmake was included" )
+message(DEBUG "Utils::FetchDependencies.cmake was included")
 
 function(Core_SuppressAllSubmoduleWarnings Target)
     target_compile_options(${Target} PUBLIC
@@ -10,10 +10,10 @@ function(Core_SuppressAllSubmoduleWarnings Target)
 endfunction()
 
 FetchContent_Declare(Glm
-        GIT_REPOSITORY https://github.com/g-truc/glm.git
-        GIT_TAG 1.0.1
-        GIT_SHALLOW TRUE
-        GIT_PROGRESS TRUE
+    GIT_REPOSITORY https://github.com/g-truc/glm.git
+    GIT_TAG 1.0.1
+    GIT_SHALLOW TRUE
+    GIT_PROGRESS TRUE
 )
 set(GLM_TEST_ENABLE OFF CACHE BOOL "" FORCE)
 
@@ -22,10 +22,10 @@ Core_SuppressAllSubmoduleWarnings(glm)
 
 
 FetchContent_Declare(GoogleTest
-        GIT_REPOSITORY https://github.com/google/googletest.git
-        GIT_TAG v1.17.0
-        GIT_SHALLOW TRUE
-        GIT_PROGRESS TRUE
+    GIT_REPOSITORY https://github.com/google/googletest.git
+    GIT_TAG v1.17.0
+    GIT_SHALLOW TRUE
+    GIT_PROGRESS TRUE
 )
 set(INSTALL_GTEST OFF CACHE BOOL "" FORCE)
 
@@ -33,24 +33,22 @@ FetchContent_MakeAvailable(GoogleTest)
 Core_SuppressAllSubmoduleWarnings(gtest)
 
 
-
 FetchContent_Declare(Benchmark
-        GIT_REPOSITORY https://github.com/google/benchmark.git
-        GIT_TAG v1.9.4
-        GIT_SHALLOW TRUE
-        GIT_PROGRESS TRUE
+    GIT_REPOSITORY https://github.com/google/benchmark.git
+    GIT_TAG v1.9.4
+    GIT_SHALLOW TRUE
+    GIT_PROGRESS TRUE
 )
 
 FetchContent_MakeAvailable(Benchmark)
 Core_SuppressAllSubmoduleWarnings(benchmark)
 
 
-
 FetchContent_Declare(PCRE2
-        GIT_REPOSITORY https://github.com/PCRE2Project/pcre2.git
-        GIT_TAG pcre2-10.45
-        GIT_SHALLOW TRUE
-        GIT_PROGRESS TRUE
+    GIT_REPOSITORY https://github.com/PCRE2Project/pcre2.git
+    GIT_TAG pcre2-10.45
+    GIT_SHALLOW TRUE
+    GIT_PROGRESS TRUE
 )
 set(PCRE2_BUILD_TESTS OFF CACHE BOOL "" FORCE)
 set(PCRE2_BUILD_PCRE2GREP OFF CACHE BOOL "" FORCE)
