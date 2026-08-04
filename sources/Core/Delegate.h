f2de3c06bbd10d8f5d59827346119b575fd9a350
Valerii Koniushenko <Valerii.Koniushenko@gmail.com>
2026-08-04 21:12:27 +0200

    refactor(Core): remove redundant subscribe methods and simplify Delegate tests
    - Removed `subscribe` methods from the `Core` namespace in `Delegate.h` to streamline the interface, retaining only `subscribeAndGetID` for subscribing with object references while providing an ID.
    - Deleted the `SubscribeWithoutIDGetting` test case and the `DelegateFixture` class in `DelegateTest.cpp`, along with several related test cases, to reduce the complexity and scope of testing for `Core::Delegate`, indicating a shift towards a more focused testing strategy.

--- sources/Core/Delegate.h
+++ sources/Core/Delegate.h
@@ -122,35 +122,6 @@ namespace Core
             return id;
         }
 
-        void subscribe(CallbackT&& callback)
-        {
-            ID id(nullptr, ++_generatedID);
-            _callbacks.emplace(id.getId(), std::forward<CallbackT>(callback));
-        }
-
-        template<class RefObjectT, class ClassFuncT>
-        void subscribe(RefObjectT* object, ClassFuncT func)
-        {
-            ID id(nullptr, ++_generatedID);
-            _callbacks.emplace(id.getId(), [object, func]<class... TArgs>(TArgs&&... args)
-                               { std::invoke(func, *object, std::forward<TArgs>(args)...); });
-        }
-
-        template<class RefObjectT, class ClassFuncT>
-        void subscribe(const IntrusivePtr<RefObjectT>& object, ClassFuncT func)
-        {
-            ID id(nullptr, ++_generatedID);
-            _callbacks.emplace(
-                id.getId(),
-                [weak = WeakPtr<RefObjectT>(object), func]<class... TArgs>(TArgs&&... args)
-                {
-                    if (auto&& ptr = weak.tryLoad())
-                    {
-                        std::invoke(func, *ptr, std::forward<TArgs>(args)...);
-                    }
-                });
-        }
-
         template<class RefObjectT, class ClassFuncT>
         [[nodiscard]] ID subscribeAndGetID(RefObjectT* object, ClassFuncT func)
         {
